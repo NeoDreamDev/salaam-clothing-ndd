13a47ded6a5b35b462d988ed32ac4c43f2aba3fc
neodreamdev <salaamwilliams@gmail.com>
2023-08-21 12:20:23 -0400

    created the directory component

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import './categories.styles.scss';
+// import CategoryItem from './components/category-item/category-item.component';
+import Directory from './components/directory/directory.component';
+
+import './components/directory/directory.styles.scss';
 
 const App = () => {
   const categories = [
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://source.unsplash.com/random/?hat,hats',
     },
     {
       id: 2,
-      title: 'Jakcets',
+      title: 'Jackets',
+      imageUrl: 'https://source.unsplash.com/random/?jackets,coat',
     },
     {
       id: 3,
       title: 'Sneakers',
+      imageUrl: 'https://source.unsplash.com/random/?shoes,sneakers',
     },
     {
       id: 4,
       title: 'Womens',
+      imageUrl: 'https://source.unsplash.com/random/?women,fashion',
     },
     {
       id: 5,
       title: 'Mens',
+      imageUrl: 'https://source.unsplash.com/random/?men,fashion',
     },
   ];
 
   return (
-    <div className='catergories-container'>
-      {categories.map(({ title, id }) => (
-        <div key={id} className='category-container'>
-          <div className='bg-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+    <Directory categories={categories} />
+    // <div className='categories-container'>
+    //   {categories.map((category) => (
+    //     <CategoryItem key={category.id} category={category} />
+    //   ))}
+    // </div>
   );
 };
 
