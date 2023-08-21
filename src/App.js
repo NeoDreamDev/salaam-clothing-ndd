e005b70bbd8a31a6ba5e760aae276f1e369a1579
neodreamdev <salaamwilliams@gmail.com>
2023-08-21 11:02:57 -0400

    Adding the SASS styling for the categories

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './categories.styles.scss';
+
 const App = () => {
   const categories = [
     {
@@ -24,8 +26,8 @@ const App = () => {
 
   return (
     <div className='catergories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
+      {categories.map(({ title, id }) => (
+        <div key={id} className='category-container'>
           <div className='bg-image' />
           <div className='category-body-container'>
             <h2>{title}</h2>
