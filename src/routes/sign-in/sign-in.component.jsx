53f91e6a27736fc9e0d5c7846fa277abfb8a4561
neodreamdev <salaamwilliams@gmail.com>
2023-08-24 18:38:18 -0400

    authenticating with firebase and creating the sign in method for users authentication

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,5 +1,16 @@
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
+
 const SignIn = () => {
-  return <div>Sign in page</div>;
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+  return (
+    <div>
+      <h1>sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with google popup</button>
+    </div>
+  );
 };
 
 export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCbF8hr0yRTJEeaw9UAcUmZdws2pqvUfG4',
+  authDomain: 'salaam-clothing-954df.firebaseapp.com',
+  projectId: 'salaam-clothing-954df',
+  storageBucket: 'salaam-clothing-954df.appspot.com',
+  messagingSenderId: '535924292439',
+  appId: '1:535924292439:web:8fff150d063a936d31dc12',
+  measurementId: 'G-78Z2GNVG41',
+};
+
+// Initialize Firebase
+
+const App = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
